67a958feddd4962b81d3f109302cab5efe04b1c4
Shum Hiu Tung <mike.sam.hell@gmail.com>
2019-04-21 23:51:52 +0800

    Reverse Center css

--- Boardgame/src/Game/BigTwo/components/Center/Center.jsx
+++ Boardgame/src/Game/BigTwo/components/Center/Center.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import CardFront from '../Card/CardFront';
+import React from "react";
+import CardFront from "../Card/CardFront";
 
 export default function Center(props) {
   const { history } = props.G;
 
   return (
-    <div className="center">
+    <div className={"center " + (props.G.reverse ? "reverse" : "")}>
       <div>
         <div className="last-hand">
-          {history.hand ? history.hand.map(card => <CardFront card={card} key={card} />) : ''}
+          {history.hand
+            ? history.hand.map(card => <CardFront card={card} key={card} />)
+            : ""}
         </div>
-        <div className="message">{props.isActive ? 'Your Turn' : `Waiting for Player ${props.ctx.currentPlayer}`}</div>
+        <div className="message">
+          {props.isActive
+            ? "Your Turn"
+            : `Waiting for Player ${props.ctx.currentPlayer}`}
+        </div>
       </div>
     </div>
   );
