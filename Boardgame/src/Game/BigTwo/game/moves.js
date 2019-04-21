be7c9f3f746ac214b4892fe4523e14d0a861b0dd
Keith <suenkaleongkeith@gmail.com>
2019-04-21 18:53:17 +0800

    after king loss, reverse

--- Boardgame/src/Game/BigTwo/game/moves.js
+++ Boardgame/src/Game/BigTwo/game/moves.js
@@ -1,11 +1,9 @@
 import ready from './moves/ready';
 import playCard from './moves/playCard';
-import playCard_KP from "./moves/playCard_KP";
 import pass from './moves/pass';
 
 export default {
   ready,
   playCard,
-  playCard_KP,
   pass
 };
