63cba6af0227d8a5d73040cc652e45f8ead1eddd
kaleongsuen <36348401+kaleongsuen@users.noreply.github.com>
2019-05-08 18:55:06 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,6 +4,6 @@ const { REACT_APP_REMOTE_SERVER, REACT_APP_REMOTE_SERVER_PORT } = process.env;
 
 export default function getAPI(gameName) {
   return axios.create({
-    baseURL: `${REACT_APP_REMOTE_SERVER}:${Number(process.env.PORT)}/games/${gameName}/`
+    baseURL: `${REACT_APP_REMOTE_SERVER}:${Number(REACT_APP_REMOTE_SERVER_PORT)+1}/games/${gameName}/`
   });
 }
